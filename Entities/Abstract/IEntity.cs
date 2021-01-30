7c63d2b19909c4800af1ac98c4ac5f5b8f39b1c2
bgmgnr <begmgner13@hotmail.com>
2021-01-31 00:25:30 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{   
+    //IENtity implement eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+
+    }
+}
